0114734c8dea5177d96de2cfc5f6a1f4131fc9bc
wangcaifeng <piaoerxiaofeng@126.com>
2015-06-10 09:00:23 +0800

    new

--- creatserver.js
+++ creatserver.js
@@ -8,44 +8,90 @@ var mime = require("mime"),    //MIME类型
     less=require('less'),
     querystring = require("querystring");
     var path = require('path');
-    less.render('.class { width: (1 + 1) }', function (e, css) {
-      console.log(css);
-    });
+   
 
 var CONF = [
   {
     path : "",
-    port : 2850
+    port : 2850,
+    agent : "xuan.news.cn"
   },
   {
     path : "D:\\mobile\\",
-    port : 8888
+    port : 8888,
+    agent : "xuan.news.cn"
+  },
+   {
+    path : "C:\\Users\\piaozai\\Desktop\\buzhangzhisheng",
+    port : 8090,
+    agent : "xuan.news.cn"
   }
 ]
 function start(conf){
   var server = http.createServer(function(req,resp){
-
-    //console.log(__dirname)
       var root = conf.path||__dirname+'/';//creatserver.js所在dirname目录
-      //console.log(url.parse(req.url,true))
           pathname = url.parse(req.url).pathname;
-          //console.log(url.parse(req.url).pathname)
       try{pathurl = decodeURI(url.parse(req.url).pathname); }catch(e){ pathurl = req.url; }
       var pathname = (pathurl === '/') ? (root) : root + pathurl;  //根目录时，追加welcome页面
           req.data =querystring.parse(url.parse(req.url).query);
-         // console.log(url.parse(req.url))
-         // console.log(querystring.stringify({ foo: 'bar', baz: ['qux', 'quux'], corge: '' }))
           req.$ = {title: pathurl, fileList: [], util:'http://127.0.0.1:2850/',root:root};
           var DEBUG = req.data.debug === "true"
       fs.stat(pathname, function(error, stats){
         if(error){
           if(!fs.existsSync(pathname)){
-            var creats = fs.mkdirSync('cloudc', 0777);
+            var cookies = {};
+            headers = {   //拼接headers数据，只处理必要的
+               "user-agent": req.headers["user-agent"]|| "f2eBrowser",
+               "content-type": req.headers["content-type"] || "text/html",
+                cookie: cookies['xuan.news.cn']|| "",
+                host: 'xuan.news.cn',
+                accept: req.headers.accept || "*"
+              };
+            var param={
+                host:'xuan.news.cn',
+                port:'80',
+                path: url.parse(req.url).path,
+                method: req.method,
+                headers:headers
+            }
 
-           // console.log(pathname)
-            //fws = fs.createWriteStream(pathname);
-            
+            if(req.method == 'POST'){
+              param.headers['content-length'] = req.headers["content-length"];
+            }
+
+            var reqs= http.request(param,function(res){
+              var ck = res.headers["set-cookie"],
+                  hck = cookies['xuan.news.cn'];
+                  console.log( res.headers["set-cookie"])
+              if(ck){ // 远程cookie同步
+                [].slice.call(ck).forEach(function(item){
+                  var m = item.split(";")[0],
+                      key = m.split("=")[0],
+                      reg = new RegExp('\\b' + key + '=[^;]*');
+                  if( reg.test(hck) ){
+                    hck = hck.replace(reg,m);
+                  }else{
+                    hck += ";" + m;
+                  }
+                });
+                cookies['xuan.news.cn'] = hck;
+              }
+              if( res.statusCode === 302 ){ // 对于远程服务的302转发中的域名部分修改成本地域名
+                res.headers.location = res.headers.location.replace( /(https?:\/\/)[^\\\/]+/, "http://" + req.headers.host );
+              }
+              resp.writeHead(res.statusCode, res.headers);
+              res.pipe(resp);
+            }).on('error', function(e) {
+                  resp.writeHead(500, {"Content-Type": "text/html"});
+                  resp.end( e.stack.toString().replace(/\n/g,"<br>") );
+            })
+            req.on('data', function(chunk) {
+              reqs.write( chunk );  //提交POST数据
+            }).on('end', function() {
+              reqs.end();
+            });
           }
+          
         }else if(!error&& stats && stats.isDirectory && stats.isDirectory()){
               var folder = path.join( __dirname , "lib/tmpl/folder.html" );
               require('./lib/filter/directory').execute(req,resp,root,pathname,pathurl,__dirname);
@@ -60,20 +106,19 @@ function start(conf){
                  fields = {};
             form.uploadDir = root+'/tmp/';  //文件上传 临时文件存放路径 
             form.on('field',function(name, value){
-             
               fields[name] = value;
             }).on('file', function(field, file) {
                 if( file.size ){
                     files.push({name: field, file: file});
                 }
             }).on('end',function(){
-             //console.log(files)
-               files.map(function(file){
+
+              files.map(function(file){
                     fs.rename(file.file.path, form.uploadDir + file.file.name, function (err) {
                         if(err){ throw err; }
                     });
                 });
-             fs.readFile(pathname, function(err, data){
+              fs.readFile(pathname, function(err, data){
                   if(err){
                       throw err;
                   }
@@ -84,15 +129,6 @@ function start(conf){
              
             })
             form.parse(req);
-            /*var rs =fs.createReadStream(pathname), str = '';
-            rs.on('error',function(err){
-              resp.writeHead(500,{'Content-Type' : 'text/html'});
-              resp.end(JSON.stringify(err))
-            }).on('data',function(d){
-              rs += d;
-            }).on('end',function(){
-             resp.end(rs)
-            })*/
 
           }else{
               resp.writeHead(200, {
@@ -132,7 +168,8 @@ function start(conf){
       });
   });
   server.listen(conf.port);
-    return server;
+  return server;
+
 }
 var ports = {};
 for(var k in CONF){
